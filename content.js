ef05d01c7ab77509accf4b8efa21ef3ac0b22b2d
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2020-01-08 18:20:37 -0600

    End of workday
    SUCCESS! I have successfully implemented VERY intelligent code to dynamically determine whether a Power BI Report is an Import-Mode report or a Direct Query report. Based off the results of that code, it then dynamically determines how to refresh the report without overloading the various data sources.
    
    This was a huge step for me today. I am now able to navigate back and forth between pages. Finding out how to programatically navigate to a dataset's Settings page was really cool.
    
    One of the main objectives left at hand is replacing the "hack" setTimeout functions with the proper DOM-listening MutationSummary functions.
    
    GREAT day of programming!

--- content.js
+++ content.js
@@ -1,23 +1,34 @@
 var importModeDOMChangedWaitTime = 2;
+var extensionName = "Power BI Real-Time Slideshow";
+var connectivityMode;
 var slideTime;
 var refreshTime;
 var elapsedTime = 0;
 var execute = 1;
 
-$(window).on("load", function () {
+/*
+For Jquery Mutation Summary Root Node, use $("div.quickAccessPanePlaceHolder")
+e.g. $("div.quickAccessPanePlaceHolder").mutationSummary("connect", callback, [{ all: true }])
+*/
+
+/*
+ For Jquery Mutation Summary Root Node when navigating from dataset Settings page back to report page, use $("title")
+ e.g. $("title").mutationSummary("connect", callback, [{ all: true }])
+ */
+
+
+function ImportRefresh() {
 
     // Retrieve the name of the currently selected report from the top left-hand corner of the page.
-    var $spans = $("span[ng-bind='breadcrumb.label'][class='pbi-fcl-np']").last();
+    var $spans = $("span.pbi-fcl-np[ng-bind='breadcrumb.label']").last();
     var reportTitle = $spans.text();
 
     // Expand the navigation pane for the current Workspace.
     $("button.expanderButton", "div.paneExpanderHeader").click();
 
-    // JQuery does not have an event listener for when particular assets are loaded into the DOM and are then ready.
-    // Therefore, the Wait Time must be approximated until a better solution is found.
     setTimeout(function () {
 
-        // Show hidden list elements on the left-hand side navigation pane.
+        // Show hidden list elements on the quick access navigation pane.
         $("li").show();
 
         // Filter navigation pane's DOM to Dataset list.
@@ -45,66 +56,159 @@ $(window).on("load", function () {
 
             console.log("Refreshed. Closing popup menu.")
 
-            // Close the popup menu.
-            $datasetEllipsisButton.click();
+        }, 4000);
+
+    }, importModeDOMChangedWaitTime * 1000)
+
+}
+
+
+function DetermineConnectivityMode() {
+//window.onload = function () {
+
+    // Retrieve the name of the currently selected report from the top left-hand corner of the page.
+    var $spans = $("span.pbi-fcl-np[ng-bind='breadcrumb.label']").last();
+    var reportTitle = $spans.text();
+
+    console.log("Expanding quick access navigation pane.");
+
+    // Expand the navigation pane for the current Workspace.
+    $("button.expanderButton", "div.paneExpanderHeader").click();
+
+    setTimeout(function () {
+
+        console.log(">>> Navigating to dataset's Settings page.");
+
+        // Show hidden list elements on the quick access navigation pane.
+        $("li").show();
+
+        // Filter navigation pane's DOM to Dataset list.
+        var $datasetItems = $("button.headerButton[aria-label='Datasets']").next();
+
+        // Dynamically find the list item corresponding to the currently opened report.
+        var $datasetName = $datasetItems.find("li.item.ng-star-inserted[title='" + reportTitle + "']");
+
+        // Find the ellipsis button corresponding to the list item.
+        var $datasetEllipsisButton = $datasetName.find("button.mat-menu-trigger.openMenu");
+        // Click the ellipsis button to load the popup menu.
+        $datasetEllipsisButton.click();
+
+        setTimeout(function () {
+
+            // Navigate the DOM to find the buttons on the popup menu.
+            var $matMenuButtons = $("div.mat-menu-content").find("button");
+
+            // Find the dataset's Refresh Button.
+            var $settingsButton = $matMenuButtons.find("span:contains('Settings')").parent();
+
+            // Click the Refresh Button.
+            $settingsButton.click();
+
+            setTimeout(function () {
+
+                $importCheck = $("span.refreshSectionTitle:contains('Scheduled refresh')");
+                $directQueryCheck = $("span.refreshSectionTitle:contains('Scheduled cache refresh')");
+
+                if ($importCheck.length) {
+                    console.log("Connectivity Mode Determined: Import. \n>>> Navigating back to the report.");
+                    chrome.storage.sync.set(
+                        { connectivityMode: "Import" },
+                        function () {
+                            setTimeout(function () {
+                                window.history.back();
+
+                                setTimeout(function () {
+                                    location.reload();
+                                }, 5000);
+
+                            }, 5000);
+                        }
+                    );
+                }
+                else if ($directQueryCheck.length) {
+                    console.log("Connectivity Mode Determined: Direct Query. \n>>> Navigating back to the report.");
+                    chrome.storage.sync.set(
+                        { connectivityMode: "Direct Query" },
+                        function () {
+                            setTimeout(function () {
+                                window.history.back();
+
+                                setTimeout(function () {
+                                    location.reload();
+                                }, 5000);
+
+                            }, 5000);
+                        }
+                    );
+                }
+                else {
+                    alert("Error: Connectivity Mode could not be determined.");
+                }
+
+            }, 4000);
 
         }, 4000);
 
     }, importModeDOMChangedWaitTime * 1000)
 
-})
+}
 
 
-/*
 window.onload = function () {
 
     // Retrieve values from chrome.storage and update global variables. Include default values for the chrome.storage pairs.
     chrome.storage.sync.get(
-        { execute_trigger: 0, slide_time: 25, refresh_time: 180 }
-        ,
+        { execute_trigger: 0, slide_time: 25, refresh_time: 60, connectivityMode: "undetermined" },
         function (items) {
             slideTime = items.slide_time;
             refreshTime = items.refresh_time;
+            connectivityMode = items.connectivityMode;
             execute = items.execute_trigger;
-            beginSlideShow();
+
+            if (execute === 1 && connectivityMode === "undetermined") {
+                DetermineConnectivityMode();
+            }
+            else if (execute === 1 && (connectivityMode === "Import" || connectivityMode === "Direct Query")) {
+                firstFullScreen();
+            }
         }
     );
-
-}
-
-function beginSlideShow() {
-
-    if (execute === 1) {
-        firstFullScreen();
-    }
 
 }
 
 function firstFullScreen() {
 
     // It is necessary to separate the first Full Screen click from the other recursive clicks.
-    angular.element(document.getElementsByClassName("enterFullScreenBtn")).click();
-    SlideShowLoop();
+    $(document).find("button.enterFullScreenBtn").click();
+
+    if (connectivityMode === "Import") {
+        ImportSlideShowLoop();
+    }
+    else if (connectivityMode === "Direct Query") {
+        DirectQuerySlideShowLoop();
+    }
 
 }
 
-function SlideShowLoop() {
+function ImportSlideShowLoop() {
 
     // The setInterval function creates an infinite loop which is desired.
     setInterval(
         function () {
 
-            angular.element(document.getElementsByClassName("fullScreenNext floatingViewBtn")).click()
-            angular.element(document.getElementsByClassName("exitFullScreenBtn floatingViewBtn")).click()
+            $(document).find("button.fullScreenNext").click()
+            $(document).find("button.exitFullScreenBtn").click()
 
             // This conditional statement prevents the data source(s) from being overloaded by constant querying.
             if (elapsedTime > refreshTime) {
-                angular.element(document.getElementsByClassName("refresh")).click();
+                console.log("Refreshing dataset.");
+                ImportRefresh();
                 // Reset the elapsed time if the Refresh Button is clicked.
                 elapsedTime = 0;
             }
 
-            angular.element(document.getElementsByClassName("enterFullScreenBtn")).click()
+            $(document).find("button.refresh").click();
+            $(document).find("button.enterFullScreenBtn").click();
 
             // Accumulate the elapsed time from the previous refresh.
             elapsedTime = elapsedTime + parseInt(slideTime, 10);
@@ -114,4 +218,31 @@ function SlideShowLoop() {
     )
 
 }
-*/
+
+function DirectQuerySlideShowLoop() {
+
+    // The setInterval function creates an infinite loop which is desired.
+    setInterval(
+        function () {
+
+            $(document).find("button.fullScreenNext").click()
+            $(document).find("button.exitFullScreenBtn").click()
+
+            // This conditional statement prevents the data source(s) from being overloaded by constant querying.
+            if (elapsedTime > refreshTime) {
+                console.log("Refreshing dataset.");
+                $(document).find("button.refresh").click();
+                // Reset the elapsed time if the Refresh Button is clicked.
+                elapsedTime = 0;
+            }
+
+            $(document).find("button.enterFullScreenBtn").click();
+
+            // Accumulate the elapsed time from the previous refresh.
+            elapsedTime = elapsedTime + parseInt(slideTime, 10);
+        },
+
+        slideTime * 1000
+    )
+
+}
